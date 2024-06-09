a7374daca74648a3cb68bb5526d0be85ca0333fa
Idir FOUQUET <fouquetidir@gmail.com>
2024-06-09 19:26:50 +0200

    Fixing missing package deployment 2

--- link-sharing-app-front/src/components/Navbar.jsx
+++ link-sharing-app-front/src/components/Navbar.jsx
@@ -1,35 +1,22 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import AccountCircle from "@mui/icons-material/AccountCircle";
-import Switch from "@mui/material/Switch";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormGroup from "@mui/material/FormGroup";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
 import "../styles/components/Navbar.css";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { AuthContext } from "../context/auth.context";
 
 
 export default function MenuAppBar() {
-  const [auth, setAuth] = React.useState(true);
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [userData, setuserData] = useState(null);
 
 
   const API_URL = "http://localhost:5005";
-  const { storeToken, authenticateUser } = useContext(AuthContext);
-
-
-  const handleChange = (event) => {
-    setAuth(event.target.checked);
-  };
 
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
