feef30dab52e3676b66cc14b6c46ef3193094672
Nawelchafi <chafinawel@gmail.com>
2024-06-25 21:54:20 +0200

    adding  private

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 
 import './styles/pages/App.css'
 import { Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
 import SignupValidationPage from './pages/SignupValidationPage'
 import UpdateUserPage from './pages/UpdateUserPage'
 import BuilderPage from './pages/BuilderPage'
 import VisitorsPage from './pages/VisitorsPage'
-
+import Private from './components/Private'
 function App() {
 
   return (
