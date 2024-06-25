ea267f2f3b6c2fa2b63d2530ae659cdcdec94bcb
Idir FOUQUET <fouquetidir@gmail.com>
2024-06-25 22:04:07 +0200

    Fixing domain name fetch

--- src/pages/PreviewPage.jsx
+++ src/pages/PreviewPage.jsx
@@ -7,7 +7,7 @@ import Modal from 'react-modal';
 Modal.setAppElement('#root');
 
 function PreviewPage() {
-    const API_URL = "http://localhost:5005";
+    const API_URL = import.meta.env.VITE_BASE_URL;
 
     const inputRef = useRef(null); // ref for the input field
 
