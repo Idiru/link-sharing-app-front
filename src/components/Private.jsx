6bd9c54f2d9f14bb44ca35f20fe971786b14447f
Idir FOUQUET <fouquetidir@gmail.com>
2024-06-29 14:51:06 +0200

    Adding redirection to login page if not logged

--- src/components/Private.jsx
+++ src/components/Private.jsx
@@ -1,12 +1,21 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
+import { useNavigate } from "react-router-dom";
+
 
 function Private({ children }) {
     const { isLoggedIn, isLoading } = useContext(AuthContext);
+    const navigate = useNavigate();
+
 
     if (isLoading) return <p>Loading ...</p>;
 
-    return isLoggedIn ? <>{children}</> : null;
+    if (!isLoggedIn) {
+        // Redirect to login page if not logged in
+        navigate("/login", { replace: true });
+    }
+
+    return <>{children}</>;
 }
 
 export default Private;
