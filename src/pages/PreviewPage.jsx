f6f456d734ad9b6f64f94540ce6f73062b8b9a0f
Nawelchafi <chafinawel@gmail.com>
2024-07-05 19:34:44 +0200

    fixing preview page

--- src/pages/PreviewPage.jsx
+++ src/pages/PreviewPage.jsx
@@ -1,93 +1,75 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate, useParams } from "react-router-dom";
-import "../styles/pages/PreviewPage.css";
-import "remixicon/fonts/remixicon.css";
-import Modal from "react-modal";
-import { Link } from "react-router-dom";
-import Snackbar from "@mui/material/Snackbar";
+import '../styles/pages/PreviewPage.css';
+import 'remixicon/fonts/remixicon.css';
+import Modal from 'react-modal';
+import { Link } from 'react-router-dom';
 
-Modal.setAppElement("#root");
+Modal.setAppElement('#root');
 
 function PreviewPage() {
+
   const inputRef = useRef(null); // ref for the input field
 
-    // important social media 
-    const platforms = [
-        { platform: 'youtube', color: 'red', icon: 'ri-youtube-fill' },
-        { platform: 'github', color: 'black', icon: 'ri-github-fill' },
-        { platform: 'instagram', color: '#E1306C', icon: 'ri-instagram-fill' },
-        { platform: 'facebook', color: '#1877F2', icon: 'ri-facebook-fill' },
-        { platform: 'twitter', color: '#1DA1F2', icon: 'ri-twitter-fill' },
-        { platform: 'linkedin', color: '#0077B5', icon: 'ri-linkedin-fill' },
-        { platform: 'other', color: 'grey', icon: 'ri-link' },
+  // important social media 
+  const platforms = [
+    { platform: 'youtube', color: 'red', icon: 'ri-youtube-fill' },
+    { platform: 'github', color: 'black', icon: 'ri-github-fill' },
+    { platform: 'instagram', color: '#E1306C', icon: 'ri-instagram-fill' },
+    { platform: 'facebook', color: '#1877F2', icon: 'ri-facebook-fill' },
+    { platform: 'twitter', color: '#1DA1F2', icon: 'ri-twitter-fill' },
+    { platform: 'linkedin', color: '#0077B5', icon: 'ri-linkedin-fill' },
+    { platform: 'other', color: 'grey', icon: 'ri-link' },
 
-    ];
-    const navigate = useNavigate();
-    const navigateToPerformance = useNavigate()
-    const { id } = useParams();
+  ];
+  const navigate = useNavigate();
+  const navigateToPerformance = useNavigate()
+  const { id } = useParams();
 
   const [userData, setUserData] = useState(null);
   const [userContent, setUserContent] = useState([]);
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [shareLink, setShareLink] = useState("");
-  const [succesMessage, setsuccesMessage] = useState(undefined);
-  const [open, setOpen] = useState(false); //To handle the display of the error message toast
-
-  //To open/close the message when the user click away
-  const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-
-    setOpen(false);
-  };
-
+  const [shareLink, setShareLink] = useState('');
   const fetchUserData = async () => {
     try {
-      const response = await fetch(
-        `${import.meta.env.VITE_BASE_URL}/auth/users/${id}`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("authToken")}`,
-          },
+      const response = await fetch(`${import.meta.env.VITE_BASE_URL}/auth/users/${id}`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('authToken')}`
         }
-      );
+      });
 
       if (response.ok) {
         const result = await response.json();
         const { firstName, lastName, userName, profileImage } = result.user;
-        console.log(result.user);
+        console.log(result.user)
         setUserData({ firstName, lastName, userName, profileImage });
-        console.log(userData);
+        console.log(userData)
       } else {
-        console.error("Failed to fetch user data");
+        console.error('Failed to fetch user data');
       }
     } catch (error) {
-      console.error("Error fetching user data:", error);
+      console.error('Error fetching user data:', error);
     }
   };
 
   const fetchUserContent = async () => {
     try {
-      const response = await fetch(
-        `${import.meta.env.VITE_BASE_URL}/content/users/${id}`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("authToken")}`,
-          },
+      const response = await fetch(`${import.meta.env.VITE_BASE_URL}/content/users/${id}`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('authToken')}`
         }
-      );
+      });
 
       if (response.ok) {
         const result = await response.json();
         setUserContent(result);
       } else {
-        console.error("Failed to fetch user content");
+        console.error('Failed to fetch user content');
       }
     } catch (error) {
-      console.error("Error fetching user content:", error);
+      console.error('Error fetching user content:', error);
     }
   };
 
@@ -96,104 +78,104 @@ function PreviewPage() {
     fetchUserContent();
   }, []);
 
-    const handleRedirection = (url) => {
-        window.open(url, '_blank');
-    };
-    const handleShareLink = async () => {
-        try {
-            const response = await fetch(`${import.meta.env.VITE_BASE_URL}/content/users/${id}/publish`, {
-                method: 'PUT',
-                headers: {
-                    'Authorization': `Bearer ${localStorage.getItem('authToken')}`,
-                    'Content-Type': 'application/json'
-                }
-            });
+  const handleRedirection = (url) => {
+    window.open(url, '_blank');
+  };
+  const handleShareLink = async () => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_BASE_URL}/content/users/${id}/publish`, {
+        method: 'PUT',
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('authToken')}`,
+          'Content-Type': 'application/json'
+        }
+      });
 
       if (response.ok) {
         const result = await response.json();
-        console.log("Content published successfully");
-        setShareLink(
-          `${import.meta.env.VITE_BASE_URL_FRONT}/devlinks/${userData.userName}`
-        );
-        setsuccesMessage("All changes published successfully.");
-        setOpen(true);
+        console.log('Content published successfully');
+        setShareLink(`${import.meta.env.VITE_BASE_URL_FRONT}/devlinks/${userData.userName}`);
         setModalIsOpen(true);
       } else {
-        console.error("Failed to publish content");
+        console.error('Failed to publish content');
       }
     } catch (error) {
-      console.error("Error publishing content:", error);
+      console.error('Error publishing content:', error);
     }
   };
-  // COPYING THE LICK FROM INPUT FIELD HANDLER
+  const handleAnalytics = (contentId) => {
+    const analyticsUrl = `${import.meta.env.VITE_BASE_URL_FRONT}/performance/${contentId}`;
+    window.open(analyticsUrl, '_blank');
+  };
+  // COPYING THE LICK FROM INPUT FIELD HANDLER 
   const copyToClipboard = () => {
     if (inputRef.current) {
       inputRef.current.select();
-      document.execCommand("copy");
+      document.execCommand('copy');
     }
   };
 
-    return (
-        <div style={{ background: 'var(--Light-Grey, #FAFAFA)' }}>
-            <div className='upper-container'>
-                <div className='btn-container'>
-                    <button className='back-btn' onClick={() => navigate(`/`)}>
-                        <Link to={`${import.meta.env.VITE_BASE_URL}/`} />
-                        <p>Back To Editor</p>
-                    </button>
-                    <button className='share-btn' onClick={handleShareLink}>
-                        <p>Publish</p>
-                    </button>
-                </div>
-            </div>
-            <div>
-                <div className='info-container'>
-                    <div className='img-name'>
-                        <img src={userData?.profileImage} alt="Profile" />
-                        <h3>
-                            {userData ? (
-                                userData.firstName && userData.lastName ?
-                                    `${userData.firstName} ${userData.lastName}` :
-                                    userData.userName
-                            ) : 'Loading...'}
-                        </h3>                    </div>
-                    <div className='content-container'  >
-                        {userContent && userContent.length > 0 ? (
-                            userContent.map((content) => {
-                                const platformData = platforms.find(p => p.platform === content.platform.toLowerCase());
-                                return (
-                                    <div className='content-item' style={{ backgroundColor: platformData ? platformData.color : 'grey' }}>
-                                        {platformData && <i className={platformData.icon} style={{ color: 'white' }} />}
-                                        <h4>{content.title}</h4>
-                                        <div >
-                                            <i class="ri-bar-chart-2-line" style={{ color: 'white', }} onClick={() => handleAnalytics(content._id)} key={content._id} ></i>
-                                            <i className="ri-arrow-right-line " style={{ color: 'white', paddingLeft: '5px' }} onClick={() => handleRedirection(content.url)} key={content._id}></i>
-                                        </div>
-                                    </div>
-                                );
-                            })
-                        ) : (
-                            <p>No content available here</p>
-                        )}
-                    </div>
-                </div>
-            </div>
-            <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={() => setModalIsOpen(false)}
-                contentLabel="Share Link Modal"
-                className="modal"
-                overlayClassName="modal-overlay"
-            >
-                <h2>Share Your Link</h2>
-                <div className='copy-input'>
-
-                    <input type="text" value={shareLink} ref={inputRef} readOnly />
-                    <span onClick={copyToClipboard} ><i className="ri-links-line"></i></span>
-                </div>
-            </Modal>
+  return (
+    <div style={{ background: 'var(--Light-Grey, #FAFAFA)' }}>
+      <div className='upper-container'>
+        <div className='btn-container'>
+          <button className='back-btn' onClick={() => navigate(`/`)}>
+            <Link to={`${import.meta.env.VITE_BASE_URL}/`} />
+            <p>Back To Editor</p>
+          </button>
+          <button className='share-btn' onClick={handleShareLink}>
+            <p>Publish</p>
+          </button>
         </div>
-    );
+      </div>
+      <div>
+        <div className='info-container'>
+          <div className='img-name'>
+            <img src={userData?.profileImage} alt="Profile" />
+            <h3>
+              {userData ? (
+                userData.firstName && userData.lastName ?
+                  `${userData.firstName} ${userData.lastName}` :
+                  userData.userName
+              ) : 'Loading...'}
+            </h3>                    </div>
+          <div className='content-container'  >
+            {userContent && userContent.length > 0 ? (
+              userContent.map((content) => {
+                const platformData = platforms.find(p => p.platform === content.platform.toLowerCase());
+                return (
+                  <div className='content-item' style={{ backgroundColor: platformData ? platformData.color : 'grey' }}>
+                    {platformData && <i className={platformData.icon} style={{ color: 'white' }} />}
+                    <h4>{content.title}</h4>
+                    <div >
+                      <i class="ri-bar-chart-2-line" style={{ color: 'white', }} onClick={() => handleAnalytics(content._id)} key={content._id} ></i>
+                      <i className="ri-arrow-right-line " style={{ color: 'white', paddingLeft: '5px' }} onClick={() => handleRedirection(content.url)} key={content._id}></i>
+                    </div>
+                  </div>
+                );
+              })
+            ) : (
+              <p>No content available</p>
+            )}
+          </div>
+        </div>
+      </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        contentLabel="Share Link Modal"
+        className="modal"
+        overlayClassName="modal-overlay"
+      >
+        <h2>Share Your Link</h2>
+        <div className='copy-input'>
+
+          <input type="text" value={shareLink} ref={inputRef} readOnly />
+          <span onClick={copyToClipboard} ><i className="ri-links-line"></i></span>
+        </div>
+      </Modal>
+    </div>
+  );
 }
 
 export default PreviewPage;
--- src/pages/VisitorsPage.jsx
+++ src/pages/VisitorsPage.jsx
@@ -1,182 +1,201 @@
-import React, { useEffect, useRef, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import '../styles/pages/PreviewPage.css';
-import 'remixicon/fonts/remixicon.css';
+import React, { useEffect, useState, useRef } from 'react'
+import { useParams } from 'react-router-dom'
+import { Line, Bar } from 'react-chartjs-2'
+import 'chart.js/auto';
+import '../styles/pages/AnalyticsPage.css'
 
-function VisitorsPage() {
-  const inputRef = useRef(null); // ref for the input field
-
-  // important social media
-  const platforms = [
-    { platform: "youtube", color: "#EE3939", icon: "ri-youtube-fill" },
-    { platform: "github", color: "#1A1A1A", icon: "ri-github-fill" },
-    { platform: 'other', color: '#737373', icon: 'ri-instagram-fill' },
-    { platform: "facebook", color: "#2442AC", icon: "ri-facebook-fill" },
-    { platform: "twitter", color: "#43B7E9", icon: "ri-twitter-fill" },
-    { platform: "linkedin", color: "#2D68FF", icon: "ri-linkedin-fill" },
-    { platform: "twitch", color: "#EE3FC8", icon: "ri-link" },
-  ];
-
-  const navigate = useNavigate();
-  const { userName } = useParams();
-  const token = localStorage.getItem("authToken");
-  const decodedToken = jwtDecode(token);
-  const id = decodedToken._id;
-
-  const [userData, setUserData] = useState(null);
-  const [userContent, setUserContent] = useState([]);
-
-  const fetchUserData = async () => {
-    try {
-      const response = await fetch(
-        `${import.meta.env.VITE_BASE_URL}/auth/users/${id}`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("authToken")}`,
-          },
-        }
-      );
-
-      if (response.ok) {
-        const result = await response.json();
-        const { firstName, lastName, userName, profileImage } = result.user;
-        console.log(result.user);
-        setUserData({ firstName, lastName, userName, profileImage });
-        console.log(userData);
-      } else {
-        console.error("Failed to fetch user data");
-      }
-    } catch (error) {
-      console.error("Error fetching user data:", error);
-    }
-  };
-
-  const fetchUserContent = async () => {
-    try {
-      const response = await fetch(
-        `${import.meta.env.VITE_BASE_URL}/content/users/${id}`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("authToken")}`,
-          },
-        }
-      );
-
-      if (response.ok) {
-        const result = await response.json();
-        const newList = result.filter((item) => item.isPublished === true)
-        setUserContent(newList);
-      } else {
-        console.error("Failed to fetch user content");
-      }
-    } catch (error) {
-      console.error("Error fetching user content:", error);
-    }
-  };
+function AnalyticsPage() {
+  const { contentId } = useParams();
+  const [analyticsData, setAnalyticsData] = useState([]);
+  const [monthlyData, setMonthlyData] = useState([]);
+  const [dailyData, setDailyData] = useState([]);
+  const [totalClicks, setTotalClicks] = useState(0);
+  const [totalMonthlyClicks, setTotalMonthlyClicks] = useState(0)
+  const [totalAnnualClicks, setTotalAnnualClicks] = useState(0)
+  const [averageDailyClicks, setAverageDailyClicks] = useState(0)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const chartRef = useRef(null)
 
   useEffect(() => {
-    fetchUserData();
-    fetchUserContent();
-  }, []);
+    const fetchAnalyticsData = async () => {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_BASE_URL}/clicks/${contentId}`, {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${localStorage.getItem('authToken')}`
+          }
+        })
 
-  const handleRedirection = async (contentId, url) => {
-    try {
-      // Fetch visitor's IP address
-      const ipResponse = await fetch('https://api64.ipify.org?format=json');
-      if (!ipResponse.ok) {
-        throw new Error('Failed to fetch IP address');
+        if (response.ok) {
+          const data = await response.json()
+          console.log('Analytics data:', data) // Log data received from backend
+          setAnalyticsData(data)
+          processAnalyticsData(data)
+        } else {
+          throw new Error('Failed to fetch analytics data')
+        }
+      } catch (error) {
+        console.error('Error fetching analytics data:', error)
+        setError(error.message)
+      } finally {
+        setLoading(false)
       }
-      const { ip } = await ipResponse.json();
-
-      // Fetch visitor's country based on IP address
-      const countryResponse = await fetch(`https://ipapi.co/${ip}/json/`);
-      if (!countryResponse.ok) {
-        throw new Error('Failed to fetch country');
-      }
-      const { country } = await countryResponse.json();
-      console.log('Visitor Country:', country);
-
-      // Send POST request to record the click and store country
-      const clickResponse = await fetch(`${import.meta.env.VITE_BASE_URL}/clicks`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${localStorage.getItem('authToken')}`
-        },
-        body: JSON.stringify({ contentId, country })
-      });
-
-      if (clickResponse.ok) {
-        console.log('Click recorded and country stored');
-      } else {
-        console.error('Failed to record click and store country');
-      }
-
-      // Redirect to the URL
-      window.location.href = url;
-    } catch (error) {
-      console.error('Error handling click:', error);
     }
-  };
 
-  return (
-    <div className="preview-page-container">
-      <div className="upper-container visitor-upper"></div>
-      <div>
-        <div className="info-container">
-          <div
-            className="img-name"
-            style={{ backgroundImage: `url(${userData?.profileImage})` }}
-          >
-            {" "}
-          </div>
-          <h3>
-            {userData
-              ? userData.firstName && userData.lastName
-                ? `${userData.firstName} ${userData.lastName}`
-                : userData.userName
-              : "Loading..."}
-          </h3>
-          <div className="content-container">
-            {userContent && userContent.length > 0 ? (
-              userContent.map((content) => {
-                const platformData = platforms.find(
-                  (p) => p.platform === content.platform.toLowerCase()
-                );
-                return (
+    fetchAnalyticsData()
+  }, [contentId])
 
-                  < div key={content._id}
-                    onClick={() => handleRedirection(content._id, content.url)}
+  const processAnalyticsData = (data) => {
+    const dailyCounts = {}
+    const monthlyCounts = {}
+    let totalClicks = 0
+    let totalMonthlyClicks = 0
+    let totalAnnualClicks = 0
+    let daysWithClicks = 0
 
-                    className='content-item'
-                    style={{ backgroundColor: platformData ? platformData.color : 'grey' }}
-                  >
-                    {platformData && (
-                      <img
-                        src={`/svg/${platformData.platform}-white-logo.svg`}
-                        alt="logo"
-                      />
-                    )}
-                    <h4>{content.title}</h4>
+    data.forEach((click) => {
+      const date = new Date(click.clickTimestamp)
+      const formattedDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}` // dd/mm/yyyy
+      const month = `${date.getMonth() + 1}/${date.getFullYear()}` // mm/yyyy
 
-                    <i
-                      className="ri-arrow-right-line "
-                      style={{ color: "white" }}
-                    ></i>
-                  </div>
-                );
-              })
-            ) : (
-              <p>No content available</p>
-            )}
-          </div>
-        </div>
-      </div>
+      if (!dailyCounts[formattedDate]) {
+        dailyCounts[formattedDate] = 0
+        daysWithClicks++
+      }
+      if (!monthlyCounts[month]) {
+        monthlyCounts[month] = 0
+      }
+
+      dailyCounts[formattedDate]++
+      monthlyCounts[month]++
+      totalClicks++
+    })
+
+    const currentMonth = `${new Date().getMonth() + 1}/${new Date().getFullYear()}` // mm/yyyy
+    totalMonthlyClicks = monthlyCounts[currentMonth] || 0
+
+    const currentYear = new Date().getFullYear()
+    totalAnnualClicks = Object.keys(monthlyCounts).reduce((acc, month) => {
+      if (month.endsWith(`/${currentYear}`)) {
+        acc += monthlyCounts[month]
+      }
+      return acc
+    }, 0)
+
+    // Convert dailyCounts and monthlyCounts to arrays and sort them
+    const dailyCountsArray = Object.entries(dailyCounts).map(([date, count]) => ({ date, count }))
+    dailyCountsArray.sort((a, b) => {
+      const dateA = new Date(`${a.date.split('/')[2]}-${a.date.split('/')[1]}-${a.date.split('/')[0]}`)
+      const dateB = new Date(`${b.date.split('/')[2]}-${b.date.split('/')[1]}-${b.date.split('/')[0]}`)
+      return dateA - dateB
+    })
+
+    const monthlyCountsArray = Object.entries(monthlyCounts).map(([month, count]) => ({ month, count }))
+
+    setDailyData(dailyCountsArray)
+    setMonthlyData(monthlyCountsArray)
+    setTotalClicks(totalClicks)
+    setTotalMonthlyClicks(totalMonthlyClicks)
+    setTotalAnnualClicks(totalAnnualClicks)
+    setAverageDailyClicks(totalClicks / daysWithClicks)
+  }
+
+  const getCssVariableValue = (variable) => {
+    return getComputedStyle(document.documentElement).getPropertyValue(variable)
+  }
+
+  const createGradient = (ctx, chartArea) => {
+    const gradient = ctx.createLinearGradient(0, chartArea.bottom, 0, chartArea.top)
+    gradient.addColorStop(0, 'rgba(99,60,255,0.2)')
+    gradient.addColorStop(1, 'rgba(99,60,255,0.6)')
+    return gradient
+  }
+
+  // Prepare data for daily chart
+  const dailyLabels = dailyData.map(item => item.date)
+  const dailyCounts = dailyData.map(item => item.count)
+
+  // Prepare data for monthly chart
+  const monthlyLabels = monthlyData.map(item => item.month)
+  const monthlyCounts = monthlyData.map(item => item.count)
+
+  // Configuration for daily chart
+  const dailyChartData = {
+    labels: dailyLabels.map(date => date.split('/').reverse().join('-')), // Convert dd/mm/yyyy to yyyy-mm-dd for sorting
+    datasets: [{
+      label: 'Daily Clicks',
+      data: dailyCounts,
+      fill: true,
+      backgroundColor: (context) => {
+        const chart = context.chart
+        const { ctx, chartArea } = chart
+
+        if (!chartArea) {
+          return null
+        }
+        return createGradient(ctx, chartArea)
+      },
+      borderColor: 'rgba(99,60,255,1)',
+      tension: 0.1
+    }]
+  }
+
+  // Configuration for monthly chart
+  const monthlyChartData = {
+    labels: monthlyLabels.slice().reverse(), // Reversed array of months
+    datasets: [{
+      label: 'Monthly Clicks',
+      data: monthlyCounts.slice().reverse(), // Reversed array of counts
+      backgroundColor: getCssVariableValue('--Purple') || '#633cff',
+      barThickness: 20
+    }]
+  }
+
+  // Render summary card function
+  const renderSummaryCard = (title, mainValue, bgColor) => (
+    <div className="summary-card" style={{ backgroundColor: bgColor }}>
+      <h3>{title}</h3>
+      <div className="main-value">{mainValue}</div>
     </div>
+  )
 
-  );
+  // Return the JSX of the component
+  return (
+    <div className="analytics-page">
+      <h2>Analytics Page</h2>
+      <p>Content ID: {contentId}</p>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error: {error}</p>
+      ) : (
+        <div>
+          {analyticsData.length > 0 ? (
+            <div>
+              <div className="summary-cards-container">
+                {renderSummaryCard("Total Monthly Clicks", totalMonthlyClicks, "linear-gradient(135deg, rgba(60,150,255,0.8), rgba(60,150,255,0.6))")}
+                {renderSummaryCard("Total Annual Clicks", totalAnnualClicks, "linear-gradient(135deg, rgba(255,150,60,0.8), rgba(255,150,60,0.6))")}
+                {renderSummaryCard("Average Daily Clicks", averageDailyClicks.toFixed(2), "linear-gradient(135deg, rgba(99,60,255,0.8), rgba(99,60,255,0.6))")}
+              </div>
+              <div className="graphs-containerx">
+                <div className="chart-container">
+                  <h3>Daily Clicks</h3>
+                  <Line ref={chartRef} data={dailyChartData} />
+                </div>
+                <div className="chart-container">
+                  <h3>Monthly Clicks</h3>
+                  <Bar data={monthlyChartData} />
+                </div>
+              </div>
+            </div>
+          ) : (
+            <p>No analytics data available</p>
+          )}
+        </div>
+      )}
+    </div>
+  )
 }
 
-export default VisitorsPage;
+export default AnalyticsPage
